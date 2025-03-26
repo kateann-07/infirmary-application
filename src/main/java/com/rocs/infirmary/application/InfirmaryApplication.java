ff5ccad4b861342bda2a32b4ca4ea9aa672f3054
kim <kim.yashai.legaspi.panlilio@gmail.com>
2025-03-27 02:14:28 +0800

    Daily Treatment Record and Clinic Visit Log

--- src/main/java/com/rocs/infirmary/application/InfirmaryApplication.java
+++ src/main/java/com/rocs/infirmary/application/InfirmaryApplication.java
@@ -12,7 +12,7 @@ public class InfirmaryApplication extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
 
-        loader.setLocation(getClass().getResource("/views/AddDailyTreatmentRecord.fxml"));
+        loader.setLocation(getClass().getResource("/views/mainpage.fxml"));
 
         BorderPane mainLayout;
         mainLayout = loader.load();
