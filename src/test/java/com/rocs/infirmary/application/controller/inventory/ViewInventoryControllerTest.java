19f659c5eb7e6613a7b37fa1d528f7b62da26641
jppeji <juliuspatrick.amita.peji@gmail.com>
2025-10-07 20:55:58 +0800

    Unit test for view medicine fix (#77)

--- src/test/java/com/rocs/infirmary/application/controller/inventory/ViewInventoryControllerTest.java
+++ src/test/java/com/rocs/infirmary/application/controller/inventory/ViewInventoryControllerTest.java
@@ -1,14 +1,18 @@
 package com.rocs.infirmary.application.controller.inventory;
 
+import com.rocs.infirmary.application.app.facade.dashboard.DashboardFacade;
+import com.rocs.infirmary.application.controller.lowstock.helper.LowStockAlertHelper;
 import com.rocs.infirmary.application.data.model.inventory.medicine.Medicine;
+import com.rocs.infirmary.application.module.lowstock.notification.service.application.LowStockNotificationServiceApplication;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
-import java.util.List;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -18,31 +22,44 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
-
-
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.mockito.Mockito.*;
 
 @ExtendWith(ApplicationExtension.class)
 public class ViewInventoryControllerTest {
 
     private Button quantityButton;
-    private  Button inventory_Filter_Button_A;
-    private  Button inventory_Filter_Button_Z;
+    private Button inventoryFilterButtonA;
+    private Button inventoryFilterButtonZ;
     private TextField searchTextField;
     private Button inventoryClearFilterButton;
     private TableView<Medicine> medDetailsTable;
 
-
     @Start
     public void start(Stage stage) throws IOException {
+
+        LowStockNotificationServiceApplication lowStockNotificationServiceApplicationMock = mock(LowStockNotificationServiceApplication.class);
+        DashboardFacade dashboardFacadeMock = mock(DashboardFacade.class);
+
+        when(lowStockNotificationServiceApplicationMock.getDashboardFacade()).thenReturn(dashboardFacadeMock);
+        when(dashboardFacadeMock.getAllLowStockMedicine()).thenReturn(Collections.emptyList());
+
+        LowStockAlertHelper lowStockAlertHelper = new LowStockAlertHelper();
+        lowStockAlertHelper.bindService(lowStockNotificationServiceApplicationMock);
+        lowStockAlertHelper.bindUI(new ImageView(), new ToggleButton());
+        lowStockAlertHelper.setMainNode(new Pane());
+
+
+        LowStockAlertHelper.checkLowStockAndShowAlert();
+
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/views/InventoryPage.fxml"));
-        BorderPane mainLayout = loader.load();
+        StackPane mainLayout = loader.load();
         Scene scene = new Scene(mainLayout);
         stage.setMaximized(true);
         stage.setScene(scene);
@@ -50,21 +67,21 @@ public class ViewInventoryControllerTest {
     }
 
     @BeforeEach
-    void setup (FxRobot robot){
-        quantityButton = robot.lookup("#QuantityButton").queryAs(Button.class);
+    void setup(FxRobot robot) {
+
+        quantityButton = robot.lookup("#quantityButton1").queryAs(Button.class);
         medDetailsTable = robot.lookup("#medDetailsTable").queryAs(TableView.class);
-        inventory_Filter_Button_A = robot.lookup("#Inventory_Filter_Button_A").queryAs(Button.class);
-        inventory_Filter_Button_Z = robot.lookup("#Inventory_Filter_Button_Z").queryAs(Button.class);
-        inventoryClearFilterButton = robot.lookup("#InventoryClearFilterButton").queryAs(Button.class);
+        inventoryFilterButtonA = robot.lookup("#inventoryFilterButtonA1").queryAs(Button.class);
+        inventoryFilterButtonZ = robot.lookup("#inventoryFilterButtonZ1").queryAs(Button.class);
+        inventoryClearFilterButton = robot.lookup("#inventoryClearFilterButton1").queryAs(Button.class);
         searchTextField = robot.lookup("#searchTextField").queryAs(TextField.class);
 
         assertNotNull(searchTextField);
         assertNotNull(quantityButton);
         assertNotNull(medDetailsTable);
-        assertNotNull(inventory_Filter_Button_A);
-        assertNotNull(inventory_Filter_Button_Z);
+        assertNotNull(inventoryFilterButtonA);
+        assertNotNull(inventoryFilterButtonZ);
         assertNotNull(inventoryClearFilterButton);
-
     }
 
     @Disabled
@@ -77,16 +94,16 @@ public class ViewInventoryControllerTest {
 
     @Disabled
     @Test
-    public void testInventory_Filter_Button_A(FxRobot robot){
-        robot.clickOn(inventory_Filter_Button_A);
+    public void testInventoryFilterButtonA(FxRobot robot){
+        robot.clickOn(inventoryFilterButtonA);
         assertEquals(medDetailsTable.getItems().stream().map(Medicine::getItemName).toList(),
                 medDetailsTable.getItems().stream().map(Medicine::getItemName).sorted().toList());
     }
 
     @Disabled
     @Test
-    public void testInventory_Filter_Button_Z(FxRobot robot){
-        robot.clickOn(inventory_Filter_Button_Z);
+    public void testInventoryFilterButtonZ(FxRobot robot){
+        robot.clickOn(inventoryFilterButtonZ);
         assertEquals(medDetailsTable.getItems().stream().map(Medicine::getItemName).toList(),
                 medDetailsTable.getItems().stream().map(Medicine::getItemName).sorted(Comparator.reverseOrder()).toList());
     }
@@ -96,7 +113,7 @@ public class ViewInventoryControllerTest {
     @Test
     public void testInventoryClearFilterButton(FxRobot robot){
         List<String> originalArrangement = medDetailsTable.getItems().stream().map(Medicine::getItemName).toList();
-        robot.clickOn(inventory_Filter_Button_A);
+        robot.clickOn(inventoryFilterButtonA);
         robot.clickOn(inventoryClearFilterButton);
         List <String> afterFilterArrangment = medDetailsTable.getItems().stream().map(Medicine::getItemName).toList();
         assertEquals(afterFilterArrangment,originalArrangement);
